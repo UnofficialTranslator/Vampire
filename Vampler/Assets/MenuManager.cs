85e094420932ae4bf3354f9478fcfe5c9b8c4ff5
UnofficialTranslator <cooldaffie9@gmail.com>
2023-12-21 16:59:47 +0100

    Audio is in there
    The audio doesn't play before scene change. It does not play

--- Vampler/Assets/MenuManager.cs
+++ Vampler/Assets/MenuManager.cs
@@ -3,23 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] AudioSource source;
+    [SerializeField] AudioClip click;
+
+    private void Start()
+    {
+        source.clip = click;
+    }
     public void StartGame()//Go to level 1
     {
+        source.Play();
         SceneManager.LoadScene("Playable");
     }
 
     public void QuitGmame()//Close application
     {
+        source.Play();
         Application.Quit();
     }
 
     public void Credits()//Show who made it
     {
+        source.Play();
         SceneManager.LoadScene("Credits");
     }
 
     public void BacktoStart()//Go to main menu
     {
+        source.Play();
         SceneManager.LoadScene("Main Menu");
     }
 }
